1d53129f50090297767657dacba7c0f0306fef1b
clorin <cyrille.lorin@yahoo.fr>
2020-11-09 16:36:05 +0100

    compil

--- compile.sh
+++ compile.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #make -C libft/ fclean && make -C libft/
-gcc -Wall -Wextra -Werror -o get_next_line.o -c get_next_line.c -D BUFFER_SIZE=32
-gcc -Wall -Wextra -Werror -o main.o -c main.c
-gcc -Wall -Wextra -Werror -o get_next_line_utils.o -c get_next_line_utils.c
-gcc -o alloc-wrap.o -c alloc-wrap.c
-gcc -o test_gnl main.o get_next_line.o get_next_line_utils.o alloc-wrap.o -D BUFFER_SIZE=32
-
+gcc -g -Wall -Wextra -Werror -o get_next_line.o -c get_next_line.c -D BUFFER_SIZE=64
+gcc -g -Wall -Wextra -Werror -o main.o -c main.c
+gcc -g -Wall -Wextra -Werror -o get_next_line_utils.o -c get_next_line_utils.c
+#gcc -o alloc-wrap.o -c alloc-wrap.c
+gcc -o test_gnl main.o get_next_line.o get_next_line_utils.o -g -Wall -Werror -Wextra -D BUFFER_SIZE=32
