612ab91b3cd022b613dc6122cd06503b3b338509
clorin <clorin@student.42.fr>
2020-11-27 12:05:15 +0100

    feat : add debug arguments to compil with alloc-wrap.c

--- compile.sh
+++ compile.sh
@@ -3,7 +3,10 @@ BUFFER_SIZE=32
 clang -g -Wall -Wextra -Werror -o get_next_line.o -c get_next_line.c -D BUFFER_SIZE=$BUFFER_SIZE
 clang -g -Wall -Wextra -Werror -o main.o -c main.c -D BUFFER_SIZE=$BUFFER_SIZE
 clang -g -Wall -Wextra -Werror -o get_next_line_utils.o -c get_next_line_utils.c
-#clang -o alloc-wrap.o -c alloc-wrap.c
-#clang -o test_gnl main.o get_next_line.o get_next_line_utils.o alloc-wrap.o -g -Wall -Werror -Wextra  -ldl
-clang -o test_gnl main.o get_next_line.o get_next_line_utils.o -g -Wall -Werror -Wextra  -ldl
+clang -o alloc-wrap.o -c alloc-wrap.c
+if [ "$#" -eq  "0" ]; then
+	clang -o test_gnl main.o get_next_line.o get_next_line_utils.o -g -Wall -Werror -Wextra  -ldl
+else
+	clang -o test_gnl main.o get_next_line.o get_next_line_utils.o alloc-wrap.o -g -Wall -Werror -Wextra  -ldl
+fi
 rm -f *.o
